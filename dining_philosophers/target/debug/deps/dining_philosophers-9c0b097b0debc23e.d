71353e98345d29e05f5806eac776ea7806ddfdae
Caren Chang <calren24@gmail.com>
2018-02-28 17:42:54 -0800

    adding dining philosophers

---
+++ dining_philosophers/target/debug/deps/dining_philosophers-9c0b097b0debc23e.d
@@ -0,0 +1,5 @@
+/Users/caren/rust/projects/dining_philosophers/target/debug/deps/dining_philosophers-9c0b097b0debc23e: src/main.rs
+
+/Users/caren/rust/projects/dining_philosophers/target/debug/deps/dining_philosophers-9c0b097b0debc23e.d: src/main.rs
+
+src/main.rs:
---
+++ dining_philosophers/target/debug/dining_philosophers.d
@@ -0,0 +1,1 @@
+/Users/caren/rust/projects/dining_philosophers/target/debug/dining_philosophers.dSYM: /Users/caren/rust/projects/dining_philosophers/src/main.rs
